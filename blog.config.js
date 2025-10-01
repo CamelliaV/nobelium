c525bb0463b8946b8a67a8e4819559b4ef5c2cea
CamelliaV <cameliascript@gmail.com>
2025-10-01 11:34:54 +0800

    modifying sitemap base link

--- blog.config.js
+++ blog.config.js
@@ -2,7 +2,7 @@ const BLOG = {
   title: "CamelliaV の Blog - Public",
   author: "Camelliav",
   email: "cameliascript@gmail.com",
-  link: "https://camelliav.ip-ddns.com/",
+  link: "https://camelliav.cloudns.ch/",
   description: "Java；前端；AI；",
   lang: "zh-CN", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: "Asia/Shanghai", // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
