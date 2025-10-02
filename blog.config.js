afa3bd1cb4e9c9676073903a7797018125eeab90
CamelliaV <cameliascript@gmail.com>
2025-10-02 11:21:24 +0800

    tinker with ga

--- blog.config.js
+++ blog.config.js
@@ -35,14 +35,14 @@ const BLOG = {
   notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
   notionAccessToken: process.env.NOTION_ACCESS_TOKEN, // Useful if you prefer not to make your database public
   analytics: {
-    provider: "clarity", // Currently we support Google Analytics, Ackee, Clarity, please fill with 'ga', 'ackee' or 'clarity', leave it empty to disable it.
+    provider: "ga", // Currently we support Google Analytics, Ackee, Clarity, please fill with 'ga', 'ackee' or 'clarity', leave it empty to disable it.
     ackeeConfig: {
       tracker: "", // e.g 'https://ackee.craigary.net/tracker.js'
       dataAckeeServer: "", // e.g https://ackee.craigary.net , don't end with a slash
       domainId: "", // e.g '0e2257a8-54d4-4847-91a1-0311ea48cc7b'
     },
     gaConfig: {
-      measurementId: "", // e.g: G-XXXXXXXXXX
+      measurementId: "G-D2LBD3P085", // e.g: G-XXXXXXXXXX
     },
     clarityConfig: {
       clarityId: "tiyegorg69",
