4f41ae962c33ae4088fb9214b9e7eb817d461ccb
CamelliaV <cameliascript@gmail.com>
2025-10-05 22:01:38 +0800

    further fix clarity

--- components/Scripts.js
+++ components/Scripts.js
@@ -29,15 +29,54 @@ const Scripts = () => {
         </>
       )}
       {BLOG.analytics?.clarityConfig?.clarityId && (
+        // <Script
+        //   strategy="afterInteractive"
+        //   id="clarity"
+        //   dangerouslySetInnerHTML={{
+        //     __html: `(function(c,l,a,r,i,t,y){
+        //   c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        //   t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        //   y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        // })(window, document, "clarity", "script", "tiyegorg69");`,
+        //   }}
+        // />
         <Script
+          id="clarity-loader"
           strategy="afterInteractive"
-          id="clarity"
           dangerouslySetInnerHTML={{
-            __html: `(function(c,l,a,r,i,t,y){
-          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-        })(window, document, "clarity", "script", "tiyegorg69");`,
+            __html: `
+          window.clarity = window.clarity || function() {
+            (window.clarity.q = window.clarity.q || []).push(arguments);
+          };
+
+          !function(c, l, a, r, i, t, y) {
+            a[c]("metadata", function() {
+              a[c]("set", "C_IS", "0");
+            }, !1, !0);
+
+            if (a[c].v || a[c].t) {
+              return a[c]("event", c, "dup." + i.projectId);
+            }
+
+            a[c].t = !0;
+            t = l.createElement(r);
+            t.async = !0;
+            t.src = "https://scripts.clarity.ms/0.8.30/clarity.js";
+            y = l.getElementsByTagName(r)[0];
+            y.parentNode.insertBefore(t, y);
+            a[c]("start", i);
+            a[c].q.unshift(a[c].q.pop());
+            a[c]("set", "C_IS", "0");
+          }("clarity", document, window, "script", {
+            "projectId": "tiyegorg69",
+            "upload": "https://q.clarity.ms/collect",
+            "expire": 365,
+            "cookies": ["_uetmsclkid", "_uetvid"],
+            "track": true,
+            "content": true,
+            "dob": 2104
+          });
+        `,
           }}
         />
       )}
