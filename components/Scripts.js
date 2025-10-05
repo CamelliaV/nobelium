2e362544637050cb08f688f8d7ce2f197088b30c
CamelliaV <cameliascript@gmail.com>
2025-10-05 14:09:18 +0800

    try fix clarity

--- components/Scripts.js
+++ components/Scripts.js
@@ -30,25 +30,14 @@ const Scripts = () => {
       )}
       {BLOG.analytics?.clarityConfig?.clarityId && (
         <Script
-          strategy="lazyOnload"
+          strategy="afterInteractive"
           id="clarity"
           dangerouslySetInnerHTML={{
-            __html: `
-                (function(c, l, a, r, i, t, y) {
-                  c[a] = c[a] || function() {
-                    (c[a].q = c[a].q || []).push(arguments);
-                  };
-                  t = l.createElement(r);
-                  t.async = 1;
-                  t.src = "https://www.clarity.ms/tag/" + i;
-                  y = l.getElementsByTagName(r)[0];
-                  if (y && y.parentNode) {
-                    y.parentNode.insertBefore(t, y);
-                  } else {
-                    l.head.appendChild(t);
-                  }
-                })(window, document, "clarity", "script", "${BLOG.analytics.clarityConfig.clarityId}");
-                `,
+            __html: `(function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "tiyegorg69");`,
           }}
         />
       )}
